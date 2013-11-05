0ace86a75b2fa06169e59474723bfcf8c1f2e6c6
Selva <selva@selva.(none)>
2013-11-05 08:05:09 +0530

    URL updation

--- src/com/hackathon/sharetrip/utilities/Constants.java
+++ src/com/hackathon/sharetrip/utilities/Constants.java
@@ -8,18 +8,18 @@ import java.util.ArrayList;
  */
 public class Constants {
 	// API Keys
-	public static final String MAP_API_KEY = "AIzaSyCIAcb7hxKyFXKw4czvzvcH5wUZvhtug7s";
-	public static final String PLACES_API_KEY = "AIzaSyDgL34tFVYV65s13GS0tDUvXtewwirs33A";
+	public static final String MAP_API_KEY = "";
+	public static final String PLACES_API_KEY = "";
 	// Log
 	public static final String LOG_TAG = "ShareYourRide";
 	// Auto complete
 	public static final String PLACES_API_BASE = "https://maps.googleapis.com/maps/api/place";
 	public static final String TYPE_AUTOCOMPLETE = "/autocomplete";
 	public static final String OUT_JSON = "/json";
-	public static final String API_KEY = "AIzaSyDgL34tFVYV65s13GS0tDUvXtewwirs33A";
+	public static final String API_KEY = "";
 
 	// SERVER
-	public static final String SERVER_URL = "http://shareyourride.cloudapp.net:8080/";
+
 	public static ArrayList<String> itemsList = new ArrayList<String>();
 
 }
